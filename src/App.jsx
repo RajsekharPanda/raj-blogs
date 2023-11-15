015b3f837577412fdf9dfb2fd8b4246616924aea
RajsekharPanda <rajsekharpandawork@gmail.com>
2023-11-15 07:21:57 +0530

    .init

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './components/Header'
+import Pagination from './components/Pagination'
+import Blogs from './components/Blogs'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div>
-      hello
+      <Header/>
+      <Blogs/>
+      <Pagination/>
     </div>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+    return(
+        <div>
+            <header>
+                <h1>Raj Blogs</h1>
+            </header>
+        </div>
+    )
+}
+
+export default Header
