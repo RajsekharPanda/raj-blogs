64636376e2e61556fd39a6214b101d3c8a8f2c9a
RajsekharPanda <rajsekharpandawork@gmail.com>
2023-11-18 08:15:42 +0530

    API call successfull

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
-import { useState } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import './App.css'
 import Header from './components/Header'
 import Pagination from './components/Pagination'
 import Blogs from './components/Blogs'
+import { AppContext } from './context/AppContext'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const {fetchBlogPosts} = useContext(AppContext);
+
+  useEffect(() => {
+    fetchBlogPosts();
+  },[]);
 
   return (
     <div>
